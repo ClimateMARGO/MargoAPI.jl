c7e5ccd29335d76ef07e44ff66aea958248ae23a
Fons van der Plas <fonsvdplas@gmail.com>
2020-06-15 22:12:08 +0200

    Lower Ipopt print level

--- main.jl
+++ main.jl
@@ -8,7 +8,12 @@ include("./server.jl")
 using JuMP
 import Ipopt
 
-model = Model(Ipopt.Optimizer)
+model = Model(optimizer_with_attributes(Ipopt.Optimizer,
+"acceptable_tol" => 1.e-8, "max_iter" => Int64(1e8),
+"acceptable_constr_viol_tol" => 1.e-3, "constr_viol_tol" => 1.e-4,
+"print_frequency_iter" => 50,  "print_timing_statistics" => "no",
+"print_level" => 0,
+))
 
 @variable(model, 0 <= root)
 
@@ -28,7 +33,6 @@ end
 
 @expose function randimage(;width, height)
     img = rand(UInt8, width * height * 4)
-
     Dict(
         :img => img,
     )
